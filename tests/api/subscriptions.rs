a4135660f46d3f6a4e490062a9a9396335f81433
Keony Schröer <keony.schroer@gmail.com>
2023-01-29 16:40:46 -0300

    pendencia de confirmação

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -18,14 +18,32 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let res = app.post_subscription(body.into()).await;
 
     assert_eq!(200, res.status().as_u16());
+}
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+#[tokio::test]
+async fn subscribe_persist_new_subscriber() {
+    let app = spawn_app().await;
+
+    let body = "name=keony&email=keony.schroer%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscription(body.into()).await;
+
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
 
     assert_eq!(saved.email, "keony.schroer@gmail.com");
     assert_eq!(saved.name, "keony");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[tokio::test]
