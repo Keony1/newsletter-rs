c3d14f96478742a728781513d564d761c7a7ee74
Keony Schröer <keony.schroer@gmail.com>
2023-03-09 16:29:56 -0300

    quase login

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,8 +2,14 @@ mod health_check;
 mod subscriptions;
 mod subscription_confirm;
 mod newsletters;
+mod home;
+mod login;
+mod authentication;
 
 pub use health_check::*;
 pub use subscriptions::*;
 pub use subscription_confirm::*;
 pub use newsletters::*;
+pub use home::*;
+pub use login::*;
+pub use authentication::*;
